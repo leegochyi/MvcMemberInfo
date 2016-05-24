9186f41f2782886591b76d330dc39e015bc733b3
Chyi <Chyi@LEE0235>
2016-05-24 15:22:31 +0800

    加入專案檔案。

---
+++ MvcMemberInfo/Controllers/InfoesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMemberInfo.Models;
+
+namespace MvcMemberInfo.Controllers
+{
+    public class InfoesController : Controller
+    {
+        private InfoDBContext db = new InfoDBContext();
+
+        // GET: Infoes
+        public ActionResult Index()
+        {
+      
+
+            return View(db.Infoes.ToList());
+        }
+
+        // GET: Infoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Info info = db.Infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // GET: Infoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Infoes/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,MemberID,Name,Mobile,dtmBirth,dtmCreat,dtmUpdate")] Info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Infoes.Add(info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(info);
+        }
+
+        // GET: Infoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Info info = db.Infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: Infoes/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,MemberID,Name,Mobile,dtmBirth,dtmCreat,dtmUpdate")] Info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(info);
+        }
+
+        // GET: Infoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Info info = db.Infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: Infoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Info info = db.Infoes.Find(id);
+            db.Infoes.Remove(info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcMemberInfo/Models/Info.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Spatial;
+namespace MvcMemberInfo.Models
+{
+    public class Info
+    {
+        public int ID { get; set; }
+        [Display(Name = "會員ID")]
+        public int MemberID { get; set; }
+
+        [Display(Name = "會員姓名")]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "會員手機號碼")]
+        [RegularExpression("^09[0-9]{2}-[0-9]{3}-[0-9]{3}", ErrorMessage = "請依照此格式(09XX-XXX-XXX),輸入手機號碼")]
+        public string Mobile { get; set; }
+
+        [Display(Name = "會員生日")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true),]
+        public DateTime? dtmBirth { get; set; }
+
+        [Display(Name = "資料建立日期")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? dtmCreat { get; set; }
+
+        [Display(Name = "資料修改日期")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? dtmUpdate { get; set; }
+    }
+
+    public class InfoDBContext : DbContext
+    {
+        public DbSet<Info> Infoes { get; set; }
+    }
+}
